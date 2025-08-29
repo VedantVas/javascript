f84f159852f39201803549fbd7931a8a5d58c07c
Vedant Vashishtha <aiml202271@global.org.in>
2025-08-29 21:25:10 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,33 @@
+let fullname = "vedant vashishtha";
+console.log(fullname);
+
+x1 = null;
+y1 = undefined;
+{
+    let a =14;
+    console.log(a);
+
+}
+{
+    let a = 15;
+    console.log(a);
+}
+
+let age = 15;
+let price = 100.50;
+
+let x = BigInt("1213");
+let y = Symbol("Hello!");
+
+const student = {
+    fullname : "Vedant Vashishtha",
+    age : 21,
+    cgpa : 8.08,
+    ispass : true,
+};
+
+console.log(student.age);
+
+student.age = student.age + 1;
+
+console.log(student.age);
