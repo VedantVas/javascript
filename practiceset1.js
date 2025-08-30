cd9c9290c39f0893e91dac5823646b81416b6fad
Vedant Vashishtha <aiml202271@global.org.in>
2025-08-31 04:50:50 +0530

    Add files via upload

---
+++ practiceset1.js
@@ -0,0 +1,15 @@
+const product = {
+    title : "Ball Pen",
+    Ratings : 4,
+    Discount : 5,
+    price : 270,
+};
+
+const profile = {
+    user_name : "ShradhaKhapra",
+    total_post : 195,
+    followers : 569000,
+    following : 4,
+    isfollowed : false,
+    Bio : "Apna College/ Ex-Microsoft,DRDO, To Educate someone is the highest Privilege"
+}
